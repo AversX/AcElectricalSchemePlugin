0390917a628e0b46347c55fea73c0aef925a6e68
AversX <xemul.93@mail.ru>
2016-11-24 01:53:11 +0500

    AI

--- AcElectricalSchemePlugin/ControlSchemeClass.cs
+++ AcElectricalSchemePlugin/ControlSchemeClass.cs
@@ -18,8 +18,12 @@ namespace AcElectricalSchemePlugin
         private static int aoCount = 0;
         private static int diCount = 0;
         private static int doCount = 0;
+        private static int currentSheet = 7;
 
-        static public void fixLinks()
+        private static int[] currentPinAI = { 2, 2, 2, 2 };
+        private static int[] currentPinAO = { 2, 2, 2, 2 };
+
+        static public void drawControlScheme()
         {
             acDoc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
             editor = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Editor;
@@ -62,7 +66,7 @@ namespace AcElectricalSchemePlugin
             {
                 return;
             }
-            
+
             using (DocumentLock docLock = acDoc.LockDocument())
             {
                 using (Transaction acTrans = acDb.TransactionManager.StartTransaction())
@@ -73,17 +77,24 @@ namespace AcElectricalSchemePlugin
                     acModSpace = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
                     Line line = (Line)acTrans.GetObject(aiao.ObjectId, OpenMode.ForRead);
                     Point3d point = line.StartPoint.Y > line.EndPoint.Y ? line.StartPoint : line.EndPoint;
-                    //for (int i =0; i<aiCount; i++)
+                    for (int i = 0; i < aiCount; i++)
                     {
-                        insertModule(acTrans, acModSpace, acDb, point);
-
+                        insertAI(acTrans, acModSpace, acDb, point, i + 1);
+                        point = point.Add(new Vector3d(594, 0, 0));
                     }
                     acTrans.Commit();
                 }
             }
+            aiCount = 0;
+            aoCount = 0;
+            diCount = 0;
+            doCount = 0;
+            currentSheet = 7;
+            currentPinAI = new int[] { 2, 2, 2, 2 };
+            currentPinAO = new int[] { 2, 2, 2, 2 };
         }
 
-        private static void insertModule(Transaction acTrans, BlockTableRecord modSpace, Database acdb, Point3d point)
+        private static void insertAI(Transaction acTrans, BlockTableRecord modSpace, Database acdb, Point3d point, int moduleNumber)
         {
             ObjectIdCollection ids = new ObjectIdCollection();
             string filename = @"Data\AI.dwg";
@@ -113,11 +124,137 @@ namespace AcElectricalSchemePlugin
                         BlockReference br = new BlockReference(point, bt[blockName]);
                         modSpace.AppendEntity(br);
                         acTrans.AddNewlyCreatedDBObject(br, true);
-                        foreach(ObjectId id in br.AttributeCollection)
+                        BlockTableRecord btr = bt[blockName].GetObject(OpenMode.ForRead) as BlockTableRecord;
+                        foreach (ObjectId id in btr)
                         {
-                            AttributeReference ar = (AttributeReference)acTrans.GetObject(id, OpenMode.ForWrite);
-                            ar.TextString = "1";
+                            DBObject obj = id.GetObject(OpenMode.ForRead);
+                            AttributeDefinition attDef = obj as AttributeDefinition;
+                            if ((attDef != null) && (!attDef.Constant))
+                            {
+                                switch (attDef.Tag)
+                                {
+                                    case "3A":
+                                        {
+                                            using (AttributeReference attRef = new AttributeReference())
+                                            {
+                                                attRef.SetAttributeFromBlock(attDef, br.BlockTransform);
+                                                attRef.TextString = "3A" + (moduleNumber + 5);
+                                                br.AttributeCollection.AppendAttribute(attRef);
+                                                acTrans.AddNewlyCreatedDBObject(attRef, true);
+                                            }
+                                            break;
+                                        }
+                                    case "XP":
+                                        {
+                                            using (AttributeReference attRef = new AttributeReference())
+                                            {
+                                                attRef.SetAttributeFromBlock(attDef, br.BlockTransform);
+                                                attRef.TextString = "-XP3." + (moduleNumber + 5);
+                                                br.AttributeCollection.AppendAttribute(attRef);
+                                                acTrans.AddNewlyCreatedDBObject(attRef, true);
+                                            }
+                                            break;
+                                        }
+                                    case "+CABLE":
+                                        {
+                                            using (AttributeReference attRef = new AttributeReference())
+                                            {
+                                                attRef.SetAttributeFromBlock(attDef, br.BlockTransform);
+                                                if (moduleNumber % 2 != 0)
+                                                {
+                                                    attRef.TextString = "2L1+/1." + currentPinAI[0];
+                                                    currentPinAI[0]++;
+                                                }
+                                                else
+                                                {
+                                                    attRef.TextString = "2L1+/2." + currentPinAI[1];
+                                                    currentPinAI[1]++;
+                                                }
+                                                br.AttributeCollection.AppendAttribute(attRef);
+                                                acTrans.AddNewlyCreatedDBObject(attRef, true);
+                                            }
+                                            break;
+                                        }
+                                    case "-CABLE":
+                                        {
+                                            using (AttributeReference attRef = new AttributeReference())
+                                            {
+                                                attRef.SetAttributeFromBlock(attDef, br.BlockTransform);
+                                                if (moduleNumber % 2 != 0)
+                                                {
+                                                    attRef.TextString = "2M1+/3." + currentPinAI[2];
+                                                    currentPinAI[2]++;
+                                                }
+                                                else
+                                                {
+                                                    attRef.TextString = "2M1+/4." + currentPinAI[3];
+                                                    currentPinAI[3]++;
+                                                }
+                                                br.AttributeCollection.AppendAttribute(attRef);
+                                                acTrans.AddNewlyCreatedDBObject(attRef, true);
+                                            }
+                                            break;
+                                        }
+                                    case "LINK+":
+                                        {
+                                            using (AttributeReference attRef = new AttributeReference())
+                                            {
+                                                attRef.SetAttributeFromBlock(attDef, br.BlockTransform);
+                                                attRef.TextString = string.Format("(-XT24.6:{0}/3.6)", moduleNumber % 2 != 0 ? "1" : "2");
+                                                br.AttributeCollection.AppendAttribute(attRef);
+                                                acTrans.AddNewlyCreatedDBObject(attRef, true);
+                                            }
+                                            break;
+                                        }
+                                    case "LINK-":
+                                        {
+                                            using (AttributeReference attRef = new AttributeReference())
+                                            {
+                                                attRef.SetAttributeFromBlock(attDef, br.BlockTransform);
+                                                attRef.TextString = string.Format("(-XT24.6:{0}/3.6)", moduleNumber % 2 != 0 ? "3" : "4");
+                                                br.AttributeCollection.AppendAttribute(attRef);
+                                                acTrans.AddNewlyCreatedDBObject(attRef, true);
+                                            }
+                                            break;
+                                        }
+                                    case "WA":
+                                        {
+                                            using (AttributeReference attRef = new AttributeReference())
+                                            {
+                                                attRef.SetAttributeFromBlock(attDef, br.BlockTransform);
+                                                attRef.TextString = "WA3." + (moduleNumber + 5);
+                                                br.AttributeCollection.AppendAttribute(attRef);
+                                                acTrans.AddNewlyCreatedDBObject(attRef, true);
+                                            }
+                                            break;
+                                        }
+                                    case "XT":
+                                        {
+                                            using (AttributeReference attRef = new AttributeReference())
+                                            {
+                                                attRef.SetAttributeFromBlock(attDef, br.BlockTransform);
+                                                attRef.TextString = "-XT3." + (moduleNumber + 5);
+                                                br.AttributeCollection.AppendAttribute(attRef);
+                                                acTrans.AddNewlyCreatedDBObject(attRef, true);
+                                            }
+                                            break;
+                                        }
+                                    case "SH":
+                                        {
+                                            using (AttributeReference attRef = new AttributeReference())
+                                            {
+                                                attRef.SetAttributeFromBlock(attDef, br.BlockTransform);
+                                                attRef.TextString = "3." + currentSheet;
+                                                currentSheet++;
+                                                br.AttributeCollection.AppendAttribute(attRef);
+                                                acTrans.AddNewlyCreatedDBObject(attRef, true);
+                                            }
+                                            break;
+                                        }
+                                }
+                            }
                         }
+                        //br.ExplodeToOwnerSpace();
                     }
                 }
                 else editor.WriteMessage("В файле не найден блок с именем \"[{0}\"", blockName);
